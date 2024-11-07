64c30f73cabf1afeb1c08109994f22314321a74b
dannny <3000347170101@ingenieria.usac.edu.gt>
2024-11-06 20:04:36 -0600

    Home
    Creacion del Home del portal web

--- psi_2024/src/App.js
+++ psi_2024/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import './pages/login';
 //import Login from './pages/login';
-import Registro from './pages/Register';
+//import Registro from './pages/Register';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <div >
-      <Registro />
+      <Home />
     </div>
   );
 }
